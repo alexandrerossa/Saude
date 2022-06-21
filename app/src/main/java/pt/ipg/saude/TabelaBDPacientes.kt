085e4d04388a840b2404114ef3a4c9c02f0ee5e5
Alexandre Rossa <alexxandrerossa@gmail.com>
2022-06-21 03:15:19 +0100

    Criação TabelaPacientes ao método do Professor Noel

---
+++ app/src/main/java/pt/ipg/saude/TabelaBDPacientes.kt
@@ -0,0 +1,21 @@
+package pt.ipg.saude
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDPacientes(db: SQLiteDatabase) : TabelaBD(db, NOME) {
+    override fun cria() {
+        db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME_PACIENTE TEXT NOT NULL, $CAMPO_DATA_DE_NASCIMENTO INTEGER NOT NULL, $CAMPO_SEXO TEXT NOT NULL, $CAMPO_TIPO_CONSULTA TEXT NOT NULL, $CAMPO_DOUTOR_RESPONSAVEL TEXT NOT NULL, FOREIGN KEY (${TabelaPacientes.CAMPO_TIPO_CONSULTA}) REFERENCES ${TabelaConsultas.CAMPO_NOME_CONSULTA}, FOREIGN KEY (${TabelaPacientes.CAMPO_DOUTOR_RESPONSAVEL}) REFERENCES ${TabelaDoutores.CAMPO_NOME_DOUTOR} )")
+    }
+
+    companion object {
+        const val NOME = "doutores"
+        const val CAMPO_NOME_PACIENTE = "nome"
+        const val CAMPO_DATA_DE_NASCIMENTO = "data_de_nascimento"
+        const val CAMPO_SEXO = "sexo"
+        const val CAMPO_TIPO_CONSULTA = "tipo_consulta"
+        const val CAMPO_DOUTOR_RESPONSAVEL = "doutor_responsavel"
+
+        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_NOME_PACIENTE, CAMPO_DATA_DE_NASCIMENTO, CAMPO_SEXO, CAMPO_TIPO_CONSULTA, CAMPO_DOUTOR_RESPONSAVEL)
+    }
+}
