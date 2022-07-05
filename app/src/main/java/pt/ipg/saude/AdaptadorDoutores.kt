a2823105ea673a2ad4195245d618716909cce174
Alexandre Rossa <alexxandrerossa@gmail.com>
2022-07-05 05:23:00 +0100

    Adaptador criado para ligar ao ReciclerView

---
+++ app/src/main/java/pt/ipg/saude/AdaptadorDoutores.kt
@@ -0,0 +1,12 @@
+package pt.ipg.saude
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterDoutores : RecyclerView.Adapter<AdapterDoutores.ViewHolderDoutores>() {
+
+    class ViewHolderDoutores(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
