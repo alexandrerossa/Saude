9b1a15194974206d08a5ee275c6b795f91d290eb
Alexandre Rossa <alexxandrerossa@gmail.com>
2022-07-05 01:53:51 +0100

    Implementada a função insert do ContentProviderSaude

--- app/src/main/java/pt/ipg/saude/ContentProviderSaude.kt
+++ app/src/main/java/pt/ipg/saude/ContentProviderSaude.kt
@@ -226,7 +226,22 @@ class ContentProviderSaude : ContentProvider() {
      * @return The URI for the newly inserted item.
      */
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Not yet implemented")
+        val db = dbOpenHelper!!.writableDatabase
+
+        requireNotNull(values)
+
+        val id  = when (getUriMatcher().match(uri)) {
+            URI_DOUTORES -> TabelaBDDoutores(db).insert(values)
+            URI_PACIENTES -> TabelaBDPacientes(db).insert(values)
+            URI_CONSULTAS -> TabelaBDConsultas(db).insert(values)
+            else -> -1
+        }
+
+        db.close()
+
+        if (id == -1L) return null
+
+        return Uri.withAppendedPath(uri, "$id")
     }
 
     /**
