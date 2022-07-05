829be137e1291f2f7d82351b7339701d7689500e
Alexandre Rossa <alexxandrerossa@gmail.com>
2022-07-06 00:15:56 +0100

    Processar as opções od menu no fragmento associado ao menu

--- app/src/main/java/pt/ipg/saude/MainActivity.kt
+++ app/src/main/java/pt/ipg/saude/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import pt.ipg.saude.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -46,7 +47,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_lista_doutores, menu)
+        menuInflater.inflate(menuAtual, menu)
 
         this.menu = menu
         if (menuAtual == R.menu.menu_lista_doutores) {
@@ -59,16 +60,19 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else ->  {
-                if (DadosApp.listaDoutorFragment!!.processaOpcaoMenu(item)) {
-                    return true
-                } else {
-                    return super.onOptionsItemSelected(item)
-                }
+        val opcaoProcessada = when (item.itemId) {
+            R.id.action_settings -> {
+                Toast.makeText(this, R.string.versao, Toast.LENGTH_LONG).show()
+                true
+            }
+
+            else -> when(menuAtual) {
+                R.menu.menu_lista_doutores -> DadosApp.listaDoutorFragment!!.processaOpcaoMenu(item)
+                R.menu.menu_novo_doutor -> DadosApp.novoDoutorFragment!!.processaOpcaoMenu(item)
+                else -> false
             }
         }
+        return if(opcaoProcessada) true else super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/pt/ipg/saude/NovoDoutorFragment.kt
+++ app/src/main/java/pt/ipg/saude/NovoDoutorFragment.kt
@@ -3,6 +3,7 @@ package pt.ipg.saude
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
@@ -38,5 +39,23 @@ class NovoDoutorFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun navegaListaDoutor() {
+        // todo: navegar para a lista de enfermeiro
+    }
+
+    fun guardar() {
+        // todo: guardar enfermeiro
+    }
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_guardar_novo_doutor -> guardar()
+            R.id.action_cancelar_novo_doutor -> navegaListaDoutor()
+            else -> return false
+        }
+
+        return true
     }
 }
