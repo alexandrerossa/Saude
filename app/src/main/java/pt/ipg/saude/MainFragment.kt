4e68fc41797c25412f591dd7e98bfed376dd0b07
Alexandre Rossa <alexxandrerossa@gmail.com>
2022-07-05 05:14:51 +0100

    Alterar o nome de FirstFragment para MainPrincipalFragment

--- app/src/main/java/pt/ipg/saude/MainFragment.kt
+++ app/src/main/java/pt/ipg/saude/MainFragment.kt
@@ -11,7 +11,7 @@ import pt.ipg.saude.databinding.FragmentFirstBinding
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MainFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
